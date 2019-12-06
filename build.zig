e463483d87d9ac322662bcac86c4be64a9ec2da1
Wendigo Jaeger <wendigojaegersama@gmail.com>
2019-12-06 15:13:05 -0500

    Initial commit of GBA Hello World

---
+++ build.zig
@@ -0,0 +1,14 @@
+const Builder = @import("std").build.Builder;
+
+pub fn build(b: *Builder) void {
+    const mode = b.standardReleaseOptions();
+    const exe = b.addExecutable("GBAHelloWorld", "src/main.zig");
+    exe.setBuildMode(mode);
+    exe.install();
+
+    const run_cmd = exe.run();
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
