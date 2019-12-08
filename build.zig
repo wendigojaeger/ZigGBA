44b8dbfae8175d6b2a7c1cc6869d82ff17ef90c6
Wendigo Jaeger <wendigojaegersama@gmail.com>
2019-12-08 17:55:16 -0500

    Finally got a running "Hello World" on a real GBA by setting 3 pixel in mode 3

--- build.zig
+++ build.zig
@@ -1,14 +1,31 @@
 const Builder = @import("std").build.Builder;
+const builtin = @import("std").builtin;
+const std = @import("std");
 
 pub fn build(b: *Builder) void {
-    const mode = b.standardReleaseOptions();
     const exe = b.addExecutable("GBAHelloWorld", "src/main.zig");
-    exe.setBuildMode(mode);
-    exe.install();
 
-    const run_cmd = exe.run();
-    run_cmd.step.dependOn(b.getInstallStep());
+    exe.setTheTarget(std.Target {
+        .Cross = std.Target.Cross {
+            .arch = std.Target.Arch {
+                .thumb = std.Target.Arch.Arm32.v4t
+            },
+            .os = .freestanding,
+            .abi = .none
+        }
+    });
 
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    exe.setOutputDir("zig-cache/raw");
+    exe.setLinkerScriptPath("gba.ld");
+    exe.setBuildMode(builtin.Mode.ReleaseFast);
+
+    const buildGBARomCommand = b.addSystemCommand(&[_][]const u8 {
+        "C:\\Programmation\\Zig\\llvm+clang-9.0.0-win64-msvc-mt\\bin\\llvm-objcopy.exe", exe.getOutputPath(),
+        "-O", "binary",
+        "zig-cache/bin/GBAHelloWorld.gba",
+    });
+
+    buildGBARomCommand.step.dependOn(&exe.step);
+
+    b.default_step.dependOn(&buildGBARomCommand.step);
 }
